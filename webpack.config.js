9c5b0b610afc2c1ade37365d1ff91046ba0ad8ea
NatShulga <mrs.flowerfields@gmail.com>
2024-07-10 17:59:02 +0300

    modified and new files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,42 @@
+
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default {
-  mode: process.env.NODE_ENV || 'development',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(dirname, 'dist'),
+    port: 8080,
+    hot: true,
+    watchFiles: ['src/**/*'],
+    open: true,
+    compress: true,
+    historyApiFallback: true,
+    client: {
+      logging: 'info',
+      overlay: true,
+      progress: true,
+      reconnect: true,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'index.html' })
+  ],
+  optimization: {
+    minimize: true,
+    usedExports: true,
+  },
   module: {
     rules: [
       {
@@ -14,27 +49,26 @@ export default {
           },
         },
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
+        test: /\.(scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer],
+              },
+            },
+          },
+          'sass-loader',
+        ],
       },
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html',
-    }),
-  ],
-  output: {
-    clean: true,
   },
 };
