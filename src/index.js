0685a193b415a59523da4ed1fd73140be09f256d
NatShulga <mrs.flowerfields@gmail.com>
2024-08-14 14:02:08 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import { validateUrl } from './validators/urlValidator';
 import { state } from './state/state';
-import './styles/styles.css';
+
 
 document.addEventListener('DOMContentLoaded', () => {
     initializeFormView(state, validateUrl);
