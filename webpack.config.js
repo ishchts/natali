17b51faa593b0feb81879dc88244165080e8b121
NatShulga <mrs.flowerfields@gmail.com>
2024-08-17 09:59:11 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 
+import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { fileURLToPath } from 'url';
-import path from 'path';
-import autoprefixer from 'autoprefixer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,65 +10,28 @@ export default {
   mode: process.env.NODE_ENV || 'development',
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    filename: 'node.js',
+    filename: './src/index.js',
     path: path.resolve(__dirname, './dist/'),
     clean: true,
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true,
-    watchFiles: ['src/**/*'],
     open: true,
-    compress: true,
-    historyApiFallback: true,
-    client: {
-      logging: 'info',
-      overlay: true,
-      progress: true,
-      reconnect: true,
-    },
+    host: 'localhost',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'index.html'),
     }),
   ],
-  optimization: {
-    minimize: true,
-    usedExports: true,
-  },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [autoprefixer],
-              },
-            },
-          },
-          'sass-loader',
-        ],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
     ],
   },
