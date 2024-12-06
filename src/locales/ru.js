ec738651de0ceaa447ba32786a3881e5bf3c5f38
NatShulga <mrs.flowerfields@gmail.com>
2024-12-06 11:30:28 +0300

    add dir locales

---
+++ src/locales/ru.js
@@ -0,0 +1,31 @@
+export default {
+    translation: {
+      header: {
+        title: 'RSS агрегатор',
+        paragraph: 'Начните читать RSS сегодня!',
+      },
+      form: {
+        input: 'Ссылка RSS',
+        submit: 'Добавить',
+        errors: {
+          enterValidURL: 'Ссылка должна быть валидным URL',
+          rssExists: 'RSS уже существует',
+        },
+      },
+      main: {
+        postsTitle: 'Посты',
+        postWatch: 'Просмотр',
+        feedsTitle: 'Фиды',
+      },
+      modalWindow: {
+        close: 'Закрыть',
+        read: 'Читать полностью',
+      },
+      errors: {
+        invalidRSS: 'Ресурс не содержит валидный RSS',
+        responseError: 'Нет ответа от сервера',
+        requestError: 'Ошибка сети',
+      },
+      successfulRequest: 'RSS успешно загружен',
+    },
+  };
