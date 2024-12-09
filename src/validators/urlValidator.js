594c5199b82f2858711ad8100e8cd560f34214ce
NatShulga <mrs.flowerfields@gmail.com>
2024-12-09 16:41:14 +0300

    valid

--- src/validators/urlValidator.js
+++ src/validators/urlValidator.js
@@ -1,7 +1,7 @@
 const getTextFromHtml = (html) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
-  return doc.body.textContent.trim();
+  const regex = /<[^>]*>(.*?)<\/[^>]*>/gs;
+  const text = html.replace(regex, '$1').trim();
+  return text;
 };
 
 const createProxyUrl = (url) => {
