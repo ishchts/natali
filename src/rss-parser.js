9a205e26d9f61133cbb9a7443dd1939c4fb5713c
NatShulga <mrs.flowerfields@gmail.com>
2024-12-28 12:09:05 +0300

    linter refactoring

--- src/rss-parser.js
+++ src/rss-parser.js
@@ -1,28 +1,28 @@
 const parserRSS = (data) => {
-    const parser = new DOMParser();
-    const rssXML = parser.parseFromString(data, 'text/xml');
+const parser = new DOMParser();
+const rssXML = parser.parseFromString(data, 'text/xml');
 
-    const parserErrors = rssXML.querySelector('parser error');
-    if (parserErrors !== null) {
+const parserErrors = rssXML.querySelector('parser error');
+if (parserErrors !== null) {
     const error = parserErrors.textContent;
     throw new Error(error);
-    }
+}
 
-    const rssItems = rssXML.getElementsByTagName('item');
+const rssItems = rssXML.getElementsByTagName('item');
 
-    const posts = Array.from(rssItems).map((rssItem) => {
+const posts = Array.from(rssItems).map((rssItem) => {
     const postTitle = rssItem.querySelector('title').textContent;
     const postDescription = rssItem.querySelector('description').textContent;
     const postHref = rssItem.querySelector('link').textContent;
 
     return { postTitle, postDescription, postHref };
-    });
+});
 
-    const feedTitle = rssXML.querySelector('channel > title').textContent;
-    const feedDescription = rssXML.querySelector(
+const feedTitle = rssXML.querySelector('channel > title').textContent;
+const feedDescription = rssXML.querySelector(
     'channel > description',
-    ).textContent;
+).textContent;
 
-    return { feedTitle, feedDescription, posts };
+return { feedTitle, feedDescription, posts };
 };
 export default parserRSS;
